bd376daf7f9c5fcaa50c1ac4eb957cee5fe64f8c
Jeandre Le Roux <theblazehen@gmail.com>
2023-06-09 11:27:27 +0000

    Iteration 36 - Sucessfully ran task: Craft an iron pickaxe

---
+++ skill/code/craftIronPickaxe.js
@@ -0,0 +1,19 @@
+async function craftIronPickaxe(bot) {
+  // Check if the bot has a crafting table in the inventory
+  const craftingTable = bot.inventory.findInventoryItem(mcData.itemsByName.crafting_table.id);
+
+  // If not, mine a dark_oak_log, craft dark_oak_planks, and then craft a crafting table
+  if (!craftingTable) {
+    await mineBlock(bot, "dark_oak_log", 1);
+    await craftItem(bot, "dark_oak_planks", 1);
+    await craftItem(bot, "crafting_table", 1);
+  }
+
+  // Place the crafting table near the bot
+  const craftingTablePosition = bot.entity.position.offset(1, 0, 0);
+  await placeItem(bot, "crafting_table", craftingTablePosition);
+
+  // Craft an iron pickaxe using the crafting table
+  await craftItem(bot, "iron_pickaxe", 1);
+  bot.chat("Crafted an iron pickaxe.");
+}
