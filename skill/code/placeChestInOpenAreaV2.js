6a79dd7908f8e4a950221e7f49f2c5e2c5acf75e
Jeandre Le Roux <theblazehen@gmail.com>
2023-06-10 10:21:28 +0000

    Iteration 513 - Sucessfully ran task: Place a chest

---
+++ skill/code/placeChestInOpenAreaV2.js
@@ -0,0 +1,43 @@
+async function placeChestInOpenArea(bot) {
+  // Check if there is a chest in the bot's inventory
+  const chest = bot.inventory.findInventoryItem(mcData.itemsByName.chest.id);
+  if (!chest) {
+    bot.chat("No chest in inventory.");
+    return;
+  }
+
+  // Try to place the chest at a suitable position near the bot
+  const offsets = [new Vec3(1, 0, 0), new Vec3(-1, 0, 0), new Vec3(0, 0, 1), new Vec3(0, 0, -1)];
+  let chestPosition = null;
+  for (const offset of offsets) {
+    const position = bot.entity.position.offset(offset.x, offset.y, offset.z);
+    const block = bot.blockAt(position);
+    if (block.name === "air" && bot.blockAt(position.offset(0, -1, 0)).name !== "water") {
+      chestPosition = position;
+      break;
+    }
+  }
+
+  // If the bot cannot find a suitable position, move to a more open area and try again
+  if (!chestPosition) {
+    await exploreUntil(bot, new Vec3(1, 0, 1), 60, () => {
+      for (const offset of offsets) {
+        const position = bot.entity.position.offset(offset.x, offset.y, offset.z);
+        const block = bot.blockAt(position);
+        if (block.name === "air" && bot.blockAt(position.offset(0, -1, 0)).name !== "water") {
+          chestPosition = position;
+          return true;
+        }
+      }
+      return false;
+    });
+  }
+
+  // Place the chest at the selected position
+  if (chestPosition) {
+    await placeItem(bot, "chest", chestPosition);
+    bot.chat("Placed a chest.");
+  } else {
+    bot.chat("Cannot place a chest. No suitable position found.");
+  }
+}
